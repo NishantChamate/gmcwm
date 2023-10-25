efd43e5efb60d4c8c9f3f793418d090b7dc9a1e2
Henil Gandhi <henil.gandhi2003@gmail.com>
2023-10-25 23:23:05 +0530

    fixed bugs

--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -1,11 +1,6 @@
 import DashboardStats from "./components/DashboardStats";
 import AmountStats from "./components/AmountStats";
 import PageStats from "./components/PageStats";
-
-import UserGroupIcon from "@heroicons/react/24/outline/UserGroupIcon";
-import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
-import CircleStackIcon from "@heroicons/react/24/outline/CircleStackIcon";
-import CreditCardIcon from "@heroicons/react/24/outline/CreditCardIcon";
 import UserChannels from "./components/UserChannels";
 
 import BarChart from "./components/BarChart";
@@ -19,25 +14,25 @@ const statsData = [
   {
     title: "Total Coal",
     value: "350156.7kg",
-    
+
     description: "↗︎ 2300 (22%)",
   },
   {
     title: "Total Sales",
     value: "₹90,34,545",
-    
+
     description: "Current month",
   },
   {
     title: "Pending Deliveries",
     value: "9",
-    
+
     description: "50 in hot leads",
   },
   {
     title: "Successful deliveries in the last 3 days",
     value: "5",
-    
+
     description: "↑ 300 (18%)",
   },
 ];
@@ -68,29 +63,19 @@ function Dashboard() {
       </div>
 
       {/**-----------maps-------------     */}
-      <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
+      <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6 h-[500px]">
         <Maps />
-        <BarChart />
+
+        <div className="grid lg:grid-row-2 mt-6 grid-cols-1 gap-6">
+          <PageStats />
+          <AmountStats />
+        </div>
       </div>
 
       {/** ---------------------- Different charts ------------------------- */}
       <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
-        
-        <BarChart />
-      </div>
-
-      {/** ---------------------- Different stats content 2 ------------------------- */}
-
-      <div className="grid lg:grid-cols-2 mt-10 grid-cols-1 gap-6">
-        <AmountStats />
-        <PageStats />
-      </div>
-
-      {/** ---------------------- User source channels table  ------------------------- */}
-
-      <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
         <UserChannels />
-        
+        <BarChart />
       </div>
     </>
   );
